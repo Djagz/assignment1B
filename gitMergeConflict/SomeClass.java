163146e053c233c5e23f4dc4618209f8857b6933
dipes <Dipesh>
2016-08-20 14:22:33 -0700

    Added the file

---
+++ gitMergeConflict/SomeClass.java
@@ -0,0 +1,13 @@
+package gitMergeConflict;
+
+public class SomeClass {
+	
+	public void thoughtForTheDay(){
+		System.out.println("I couldn't repair your brakes, so I made your horn louder");
+	}
+	
+	public static void main(String[] args){
+		SomeClass sc = new SomeClass();
+		sc.thoughtForTheDay();
+
+}
