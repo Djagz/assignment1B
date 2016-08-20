c892ccf929245028beeea73e75b8a93a5a76b597
dipes <Dipesh>
2016-08-20 16:18:28 -0700

    Add multiple events

--- gitMergeConflict/SomeClass.java
+++ gitMergeConflict/SomeClass.java
@@ -1,6 +1,14 @@
 package gitMergeConflict;
 
+
 public class SomeClass {
+
+        private ArrayList<String> events = new ArrayList<String>();
+
+        public void createEvents() {
+		events.add("We're going to a movie on Saturday");
+		events.add("Study session on Sunday - Jim's house");
+	}
 	
 	public void fnOne(){
 		System.out.println("On the other hand, you have different fingers");
@@ -8,12 +16,15 @@ public class SomeClass {
 
         public void upcomingEvents(){
                 System.out.println("Upcoming Events");
-		System.out.println("Party at Jane's house tomorrow");
+		for (String event : events)
+			System.out.println(event);
+
 	}
 	
 	public static void main(String[] args){
 		SomeClass sc = new SomeClass();
 		sc.fnOne();
                 sc.upcomingEvents();
+                sc.createEvents();
 
 }
