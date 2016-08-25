a267bee807698b5fddce3b53fdebf5c5be4f0ce3
dipes <Dipesh>
2016-08-24 17:19:55 -0700

    One file changed and one file added in SimpleJava

--- gitMergeConflict/SomeClass.java
+++ gitMergeConflict/SomeClass.java
@@ -3,12 +3,6 @@ package gitMergeConflict;
 
 public class SomeClass {
 
-        private ArrayList<String> events = new ArrayList<String>();
-
-        public void createEvents() {
-		events.add("We're going to a movie on Saturday");
-		events.add("Study session on Sunday - Jim's house");
-	}
 	
 	public void fnOne(){
 		System.out.println("On the other hand, you have different fingers");
@@ -16,14 +10,11 @@ public class SomeClass {
 
         public void upcomingEvents(){
                 System.out.println("Upcoming Events - Please join us!");
-		for (String event : events)
-			System.out.println(event);
 	}
 	
 	public static void main(String[] args){
 		SomeClass sc = new SomeClass();
 		sc.fnOne();
                 sc.upcomingEvents();
-                sc.createEvents();
 
 }
